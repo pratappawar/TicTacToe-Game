e4958e15e7cc2d2712d3b555de96e3686b2462c9
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-05 13:30:48 +0530

    Added: Create Empty board

--- src/com/tic_tac_toe/TicTacToeGame.java
+++ src/com/tic_tac_toe/TicTacToeGame.java
@@ -1,18 +1,18 @@
 package com.tic_tac_toe;
 
 public class TicTacToeGame {
-
-	public char[] creatBoard() {
+	
+	public char[] creatBoard() {   
 		char board[] = new char[10];
-		for (int i = 0; i < board.length; i++) {
-			board[i]=' ';
-	}
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
 		return board;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("welcome to TicTacToe!!");
-		TicTacToeGame board = new TicTacToeGame();
-		board.creatBoard();
+		System.out.println("welcome to TicTacToe!!");//Welcome message
+		TicTacToeGame boardobj = new TicTacToeGame();//object creation
+		boardobj.creatBoard();
 	}
 }
