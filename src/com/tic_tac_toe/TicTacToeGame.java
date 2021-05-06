bf58f6c0f950599d0c92d0d5aa1c12c40cd517ae
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-06 19:09:36 +0530

    Added: free space before making the desired move a move to a desired position

--- src/com/tic_tac_toe/TicTacToeGame.java
+++ src/com/tic_tac_toe/TicTacToeGame.java
@@ -1,10 +1,9 @@
 package com.tic_tac_toe;
-
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	public String playerChoice;
-	public String computerChoice;
+	public char playerChoice;
+	public char computerChoice;
 	Scanner scan = new Scanner(System.in);
 	char board[] = new char[10];
 
@@ -26,131 +25,134 @@ public class TicTacToeGame {
 	}
 
 	// choose letter user and computer choice
-	public String chooseLetter() {
+	public char chooseLetter() {
 		// taking user input
 		System.out.println("Enter the letter X-O:");
 		String inputLetter = scan.next();
 		if (inputLetter.equals("X")) {
-			playerChoice = "X";
-			computerChoice = "O";
+			playerChoice = 'X';
+			computerChoice = 'O';
 		} else if (inputLetter.equals("O")) {
-			playerChoice = "O";
-			computerChoice = "X";
+			playerChoice = 'O';
+			computerChoice = 'X';
 		} else
 			System.out.println("Invalid symbol...."); // invalid Symbol
 		return playerChoice;
 	}
 
 	// Ability for user to make a move to a desired location in the board
-	public void playerMove() {
-		System.out.println("Where do you Want to play (1-8):");
+	public char[] playerMove() {
+		System.out.println("Where do you Want to play (1-9):");
 		int positionSelector = scan.nextInt();
 		switch (positionSelector) {
 		case 1:
-			board[1] = 'X';
+			board[1] = playerChoice;
 			break;
 		case 2:
-			board[2] = 'X';
+			board[2] = playerChoice;
 			break;
 		case 3:
-			board[3] = 'X';
+			board[3] = playerChoice;
 			break;
 		case 4:
-			board[4] = 'X';
+			board[4] = playerChoice;
 			break;
 		case 5:
-			board[5] = 'X';
+			board[5] = playerChoice;
 			break;
 		case 6:
-			board[6] = 'X';
+			board[6] = playerChoice;
 			break;
 		case 7:
-			board[7] = 'X';
+			board[7] = playerChoice;
 			break;
 		case 8:
-			board[8] = 'X';
+			board[8] = playerChoice;
 			break;
 		case 9:
-			board[9] = 'X';
+			board[9] = playerChoice;
 			break;
 		default:
 			System.out.println("invalid choice");
 		}
 		showBoard();
+		return board;
+		
 	}
       //free space before making the desired move a move to a desired location in the board
-	public void checkDesiredLocationIsFree(){
+	public void isFreeSpace(){
 		System.out.println("Enter desired location ");
 		int location = scan.nextInt();
 		switch (location) {
 		case 1:
 			if (board[1] == ' ') {
-				board[1] = 'x';
+				board[1] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 2:
 			if (board[2] == ' ') {
-				board[2] = 'x';
+				board[2] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 3:
 			if (board[3] == ' ') {
-				board[3] = 'x';
+				board[3] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 4:
 			if (board[4] == ' ') {
-				board[4] = 'x';
+				board[4] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 5:
 			if (board[5] == ' ') {
-				board[5] = 'x';
+				board[5] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 6:
 			if (board[6] == ' ') {
-				board[6] = 'x';
+				board[6] = playerChoice;
 			} else {
-				System.out.println("Enetr at valid place");
+				System.out.println("space is not free");
 			}
 			break;
 		case 7:
 			if (board[7] == ' ') {
-				board[7] = 'x';
+				board[7] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 8:
 			if (board[8] == ' ') {
-				board[8] = 'x';
+				board[8] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 			break;
 		case 9:
 			if (board[9] == ' ') {
-				board[9] = 'x';
+				board[9] = playerChoice;
 			} else {
 				System.out.println("space is not free");
 			}
 		default:
 			System.out.println("invalid position");
 		}
-
+		
 	}
-
+	
+	
 	public static void main(String[] args) {
 		System.out.println("welcome to TicTacToe!!"); // Welcome message
 		TicTacToeGame tictactoeboard = new TicTacToeGame(); // object creation
@@ -158,7 +160,7 @@ public class TicTacToeGame {
 		tictactoeboard.chooseLetter(); // method call
 		tictactoeboard.showBoard(); // method call
 		tictactoeboard.playerMove(); // method call
-		tictactoeboard.checkDesiredLocationIsFree();
+		tictactoeboard.isFreeSpace();// method call
 	}
 
 }
