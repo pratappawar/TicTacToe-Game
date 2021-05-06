c6bb170569c2a651f8514bc4310c89fbf167be22
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-06 10:21:20 +0530

    Added: Ability to take :etter from User

--- src/com/tic_tac_toe/TicTacToeGame.java
+++ src/com/tic_tac_toe/TicTacToeGame.java
@@ -4,33 +4,41 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	public String play;
+	public String playerChoice;
+	public String computerChoice;
+
+	char board[] = new char[10];
+
+	// Creating Empty Board method
 	public char[] creatBoard() {
-		// Creating Empty Board
-		char board[] = new char[10];
 		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 		return board;
 	}
-	public void chooseLetter() { // Creating method
+	
+	//choose letter user and computer choice
+	public String chooseLetter() { 
 		Scanner letter = new Scanner(System.in); // taking user input
 		System.out.println("Enter the letter X-O:");
-		String input = letter.nextLine();
-		if (input.equals("X")) {
-			play = "Computer play";              //computer symbol
-			System.out.println(play);
-		} else if (input.equals("O")) {
-			play = "player play";                //player Symbol
-			System.out.println(play);
+		String inputLetter = letter.nextLine();
+		if (inputLetter.equals("X")) {
+			playerChoice = "X";    
+			computerChoice = "O";
+		} else if (inputLetter.equals("O")) {
+			playerChoice = "O";     
+			computerChoice = "X";   
 		} else
-			System.out.println("Invalid symbol....");//invalid Symbol
+			System.out.println("Invalid symbol...."); // invalid Symbol
 		letter.close();
+
+		return playerChoice;
 	}
+
 	public static void main(String[] args) {
-		System.out.println("welcome to TicTacToe!!");// Welcome message
-		TicTacToeGame tictactoeboard = new TicTacToeGame();// object creation
-		tictactoeboard.creatBoard(); // method call
-		tictactoeboard.chooseLetter();//method call
+		System.out.println("welcome to TicTacToe!!");         // Welcome message
+		TicTacToeGame tictactoeboard = new TicTacToeGame();   // object creation
+		tictactoeboard.creatBoard();                          // method call
+		tictactoeboard.chooseLetter();                        // method call
 	}
 }
